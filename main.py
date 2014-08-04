558ecc79dd72cd3667c6febf82298dd727574787
eastWillow <eastWillow@users.noreply.github.com>
2014-08-04 15:13:04 +0800

    UID Output

--- main.py
+++ main.py
@@ -1,20 +1,44 @@
-#encoding = utf-8
-#!/usr/bin/python
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
 import RPi.GPIO as GPIO
 import MFRC522
 import signal
 
+continue_reading = True
+
+# Capture SIGINT for cleanup when the script is aborted
+def end_read(signal,frame):
+    global continue_reading
+    print "Ctrl+C captured, ending read."
+    continue_reading = False
+    GPIO.cleanup()
+
+# Hook the SIGINT
+signal.signal(signal.SIGINT, end_read)
+
+# Create an object of the class MFRC522
 MIFAREReader = MFRC522.MFRC522()
 
-print "Welcome to the RFID ID CARD SYSTEM"
-print "Press Ctrl-c to STOP"
+# Welcome message
+print "Welcome to the MFRC522 data read example"
+print "Press Ctrl-C to stop."
 
-while True:
+# This loop keeps checking for chips. If one is near it will get the UID and authenticate
+while continue_reading:
+    
+    # Scan for cards    
+    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
-		(status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+    # If a card is found
+    if status == MIFAREReader.MI_OK:
+        print "Card detected"
+    
+    # Get the UID of the card
+    (status,uid) = MIFAREReader.MFRC522_Anticoll()
 
-		if status == MIFAREReader.MI_OK:
-				print "Card detected"
-		(status,uid) = MIFAREReader.MFRC522_Anticoll()
+    # If we have the UID, continue
+    if status == MIFAREReader.MI_OK:
 
-		if status == MIFAREReader.MI_OK:
+        # Print UID
+        print "Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3])
