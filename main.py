f395e217257a5aa7e5efbb9706c0c54a02f75b62
eastWillow <eastWillow@users.noreply.github.com>
2014-08-04 14:40:03 +0800

    import RC522

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 #encoding = utf-8
 #!/usr/bin/python
+import RPi.GPIO as GPIO
+import MFRC522
+import signal
 
+MIFAREReader = MFRC522.MFRC522()
 
+print "Welcome to the RFID ID CARD SYSTEM"
+print "Press Ctrl-c to STOP"
+
+while True:
+
+		(status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+
+		if status == MIFAREReader.MI_OK:
+				print "Card detected"
+		(status,uid) = MIFAREReader.MFRC522_Anticoll()
+
+		if status == MIFAREReader.MI_OK:
