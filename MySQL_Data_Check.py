09a42f1dedbcbdfab3b47ed4214bef727686cf1e
eastWillow <bobgash2@gmail.com>
2014-08-05 14:18:04 +0800

    SELECT data Form Python ok

---
+++ MySQL_Data_Check.py
@@ -0,0 +1,26 @@
+#encoding :utf-8
+#!/usr/bin/python
+
+import MySQLdb
+import binascii
+
+db = MySQLdb.connect("127.0.0.1","pi","make","RFID")
+
+cursor = db.cursor()
+
+sql = """SELECT * FROM basicInformation WHERE UID=0x2693d55434"""
+
+cursor.execute(sql)
+
+results = cursor.fetchall()
+
+for row in results:
+		UID =  row[0].encode('hex')
+		FIRSTNAME = row[1]
+		LASTNAME = row[2]
+		SEX = row[3]
+		AGE = row[4]
+
+		print "UID = 0x%s,Your Name=%s %s,%s %s"%\
+				(UID,FIRSTNAME,LASTNAME,SEX,AGE)
+db.close()
